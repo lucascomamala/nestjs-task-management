3ada825eaccc00b55a0ba240818aaffe2dab4a46
Lucas <lucascomamala@gmail.com>
2023-09-07 08:30:08 -0600

    Auth module setup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common'
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+
+import { User } from './user.entity'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+}
