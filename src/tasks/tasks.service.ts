ccb9e5c11a1aba9f756b8acb8d099704788f940a
Lucas <lucascomamala@gmail.com>
2023-09-07 11:49:51 -0600

    Make Users Own Tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,6 +7,7 @@ import { TaskStatus } from './task-status.enum'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
+import { User } from 'src/auth/user.entity'
 
 @Injectable()
 export class TasksService {
@@ -63,13 +64,14 @@ export class TasksService {
     return found
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto
 
     const task = this.tasksRepository.create({
       title,
       description,
       status: TaskStatus.OPEN,
+      user,
     })
 
     await this.tasksRepository.save(task)
